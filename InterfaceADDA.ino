77b97af81e3a29a23efddd1b6961ec183bbf30a4
ayslenon <ayslenon.c@gmail.com>
2020-11-26 17:02:49 -0300

    added comments and optimized source code

--- InterfaceADDA.ino
+++ InterfaceADDA.ino
@@ -1,104 +1,133 @@
+// Author Ayslenon Câmara - 2019 
+// Little project made for eletrocnic instrumentation classes
+
+
+// ================================================================================================================
+// include libs for project
+// ================================================================================================================
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+
+
+// ================================================================================================================
+// hardware mapping 
+// ================================================================================================================
+
 //PORT B
-#define ADC_IN_READ   0x01
-#define ADC_OUT_READ  0x02 // ativa em 0
-#define ADC_OUT_WRITE 0x04 // ativa em 0
-#define ADC_OUT_CLOCK 0x08 // ativa em 1
-#define ADC_OUT_CLK_C 0x10 // ativa em 1
+#define ADC_IN_READ   0x01  
+#define ADC_OUT_READ  0x02 // actives in 0
+#define ADC_OUT_WRITE 0x04 // actives in 0
+#define ADC_OUT_CLOCK 0x08 // actives in 1
+#define ADC_OUT_CLK_C 0x10 // actives in 1
 
 //PORT D
-#define DAC_REG_CLK1  0x04 // ativa em 1 2
-#define DAC_REG_CLK2  0x08 // ativa em 1 3 
-#define DAC_WR1       0x10 // ativa em 0 4 
-#define DAC_WR2       0x20 // ativa em 0 5
-#define DAC_REG_SER   0x40 // ativa em 1 6
-#define DAC_REG_OEN   0x80 // ativa em 0 7
+#define DAC_REG_CLK1  0x04 // actives in 1
+#define DAC_REG_CLK2  0x08 // actives in 1 
+#define DAC_WR1       0x10 // actives in 0 
+#define DAC_WR2       0x20 // actives in 0
+#define DAC_REG_SER   0x40 // actives in 1
+#define DAC_REG_OEN   0x80 // actives in 0
 
-uint8_t valor_lido[8] = {0, 0, 0, 0, 0, 0, 0, 0}, aux = 0, valor_final = 0;
+// note: where we see "pulse" for any bit, it means change its logic value to opposite, and back again to "normal value"
+// because some bits are active in 0, as we can see above
 
+// ================================================================================================================
+// variables declaration 
+// ================================================================================================================
+
+uint8_t read_value = 0, final_value = 0;
+
+
+// ================================================================================================================
+// funcions declaration
+// ================================================================================================================
+
+// function to set pwm on arduino pin 11 with duty 50% 
+// duty calculed by OCR2A/255 * 100%
 void PWMpin11(void) {
-  OCR2A = 128;                                            // Configurar o modo de comparacao (ativar o PWM)
-  TCCR2A |= (1 << COM2A1);                                // Set modo clear
-  TCCR2A |= (1 << WGM21) | (1 << WGM20);                  // Configura a forma de onda para o modo Fast PWM
-  TCCR2B |= (1 << CS20)  | (1 << CS22);                   // Configura Prescale (divide o clock por 1024)
+  OCR2A = 128;                                            // Set pwm with compare mode
+  TCCR2A |= (1 << COM2A1);                                // Set compare to clear
+  TCCR2A |= (1 << WGM21) | (1 << WGM20);                  // Set Waveform Genertion to Fast PWM
+  TCCR2B |= (1 << CS20)  | (1 << CS22);                   // Set Prescale to divide the clock by 1024
 }
 
-int main (void) {
-  DDRB  = 0x1E;
-  PORTB = ADC_OUT_READ | ADC_OUT_WRITE;
-  DDRD  = 0xFC;
-  PORTD = DAC_WR1 | DAC_WR2;
-  PWMpin11();
-  // gasta a primeira leitura
 
+// ================================================================================================================
+// main function 
+// ================================================================================================================
+
+int main (void) {
+  // Set the inputs and outputs according to the hardware
+  DDRB  = 0x1E;
+  DDRD  = 0xFC;
+  // Start ADC and DAC bits on its normal operation
+  PORTB = ADC_OUT_READ | ADC_OUT_WRITE;
+  PORTD = DAC_WR1 | DAC_WR2;
+  // Starts the clk reference to ADC
+  PWMpin11();
+  
+  // Starting the first read, because the first is always slowly compared to next ones
+  // We make a new read by pulsing the ADC_OUT_WRITE and then ADC_OUT_READ 
   PORTB ^= ADC_OUT_WRITE;
   _delay_ms(1);
   PORTB ^= ADC_OUT_WRITE | ADC_OUT_READ;
   _delay_ms(1);
   PORTB ^= ADC_OUT_READ;
+
+  // Here we have a new reading stored on ADC 8bit Register
+
+  
+// ================================================================================================================
+// here starts the infiniy loop
+// ================================================================================================================
   while (1) {
-    // leitura periodica (habilita o adc)
-    // write
+    // We are only read the value on ADC, make a complement of 1 and send to DAC
+    // We start pulsing ADC_OUT_WRITE to start a conversion
     PORTB ^= ADC_OUT_WRITE;
     _delay_ms(1);
-    // read
+    // then set ADC_OUT_READ 
     PORTB ^= ADC_OUT_WRITE | ADC_OUT_READ;
     _delay_ms(1);
 
-    // verificação no port se o nivel logico da entrada é alto ou baixo
+    // Check if the logic level in the serial input is high or low for each bit in ADC register
+    read_value = 0;
     for (int i = 0; i < 8; i++) {
-      if (PINB & ADC_IN_READ) valor_lido[i] = 1;
-      else valor_lido[i] = 0;
-      // conta +1 no contador
+      if (PINB & ADC_IN_READ) read_value += (1<<i);
+      // Pulse ADC_OUT_CLK_C to increment one bit for the parallel to serial conversion via the 4051 IC
       PORTB ^= ADC_OUT_CLK_C;
       _delay_ms(1);
       PORTB ^= ADC_OUT_CLK_C;
       _delay_ms(1);
     }
-    // tri-state no adc
+    // Set tri-state on ADC reseting ADC_OUT_READ
     PORTB ^= ADC_OUT_READ;
-
-    valor_final = 0;
-
-    for (int i = 0; i < 8; i++) {
-      // calcula a potencia de 2 para a posição i
-      aux = 1;
-      for (int j = 0; j < i; j++) {
-        aux *= 2;
-      }
-      // acumula todos os valores pra gerar um valor de 8 bits
-      valor_final += (valor_lido[i] * aux);
-    }
-
-    valor_final = 255 - valor_final;
-
-    // fazer processo inverso, descobrir quem é o vetor de bits para o valor_final novo
-    // depois enviar para o DAC
-    //valor_final = 255 - valor_final;
-    for (int i = 0; i < 8; i++) {
-      valor_lido[i] = valor_final % 2; // resto da divisão gera 0 ou 1
-      valor_final /= 2;                // a divisão reduz 1 bit, deslocando para direita
-    }
+    
+    // make the complement of 1 from the read_value
+    final_value = 255 - read_value;
 
     for (int i = 0; i < 8; i++) {
-      // configurar o registrador, depois o DAC
-      if (valor_lido[i] == 1) PORTD |= DAC_REG_SER; // caso o bit seja 1, atualiza o PORTD para ter bit PD6 = 1
-      else PORTD &= (~DAC_REG_SER); // caso contrário, bit PD6 = 0
-      // atualiza o shift register
+      // According to the read_value, we set and reset the DAC_REG_SER
+
+      if ((read_value & (1 << i)) == (1 << i)) PORTD |= DAC_REG_SER;
+      else PORTD &= (~DAC_REG_SER);
+      
+      // then we pulse the DAC_REG_CLK1 to shift the register 74595 IC
+
       PORTD ^= DAC_REG_CLK1;
       _delay_ms(1);
       PORTD ^= DAC_REG_CLK1;
       _delay_ms(1);
     }
-    // manda os dados para a saida
+    // after all 8 bits shifted, we allow the output to send its values to the DAC
+    // by pulsing DAC_REG_CLK2
     PORTD ^= DAC_REG_CLK2;
     _delay_ms(1);
     PORTD ^= DAC_REG_CLK2;
     _delay_ms(1);
-    // habilita saida do shift register e entrada do dac
+    // And finally pulsing DAC_WR1 and then DAC_WR2
+    // To receive the 8bit incoming from register and then convert the data respectively
     PORTD ^= DAC_WR1;
     _delay_ms(1);
     PORTD ^= (DAC_WR1 | DAC_WR2);
---
+++ readme.md
@@ -0,0 +1,49 @@
+# Interface AD DA to use with uC
+
+## Chacteristics 
+- Developed for atmega328p
+- Written according to the datasheets of components
+- 2 examples of uses of DAC0832
+- Multiplexed input, using less pins
+- Made in Arduino IDE
+
+## Overview
+This project was made for a class, the focus is to understand how the ADC and DAC conversion works.
+Here I used the ADC0804 and DAC0832, and made a interface for each one to use with the Atmega328p
+The ADC0804 has 8bit parallel output, so I multiplexed its outputs to have one bit serial, and 
+made the conversion with a 4051 IC multiplexer and a 40193 IC counter.
+
+The counter receives pulses in its clock pin, incoming from the uC, and increment the 4051 selector pins
+so the 4051 will convert the 8 bit to 1 bit-serial.
+The uC will read each serial bit before a new increment on 40193.
+
+Afterwards, the uC will have the AD conversion in its memory, and we can use this however we want, in my case
+I decided to make a complement of 1, and send a 8bit value to the DAC.
+
+For the DA conversion I decided to use a 74595 shift register to store 8bit incoming from uC, and then send these 8bit to 
+the DAC0832, using it in 2 different schematics to compare its outputs
+
+The store is made by a clock incrementing the shift register, after 8 shifts the output is set to send data to the DAC
+
+I recommend you to see the datasheets, from both IC I used:
+- [ADC0804](https://www.ti.com/lit/ds/symlink/adc0804-n.pdf) 
+- [40193 Counter](https://www.ti.com/lit/ds/symlink/cd40193b.pdf?ts=1606420709126&ref_url=https%253A%252F%252Fwww.ti.com%252Fproduct%252FCD40193B)
+- [4051 Multiplexer](https://www.ti.com/lit/ds/symlink/cd4051b.pdf?ts=1606420744325&ref_url=https%253A%252F%252Fwww.google.com%252F)
+- [DAC0832](https://datasheet.octopart.com/DAC0832LCN-National-Semiconductor-datasheet-14101993.pdf)
+- [74595 Shift register](https://www.ti.com/lit/ds/scls041i/scls041i.pdf?ts=1606413365686&ref_url=https%253A%252F%252Fwww.google.com%252F)
+- [Atmega328p](https://www.sparkfun.com/datasheets/Components/SMD/ATMega328.pdf)
+
+## Circuitry
+
+The following 3 circuits presents the schematic connections made in this project.
+
+### ADC Circuit
+![ADC circuit](/img/ADC.png)
+
+### ATmega Circuit
+![ATmega circuit](/img/uC.png)
+
+### DAC circuit
+![DAC circuit - 74595](/img/DAC1.png)
+![DAC circuit - DAC1](/img/DAC2.png)
+![DAC circuit - DAC2](/img/DAC3.png)
